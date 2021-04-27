c2ef23df90367a589f23f6e6ea5f0551900b6f59
mesmith454 <mesmith454@gmail.com>
2021-04-27 11:17:38 -0700

    commit tree ce42c290116dbe25cb545fb48cac6d575afa2287

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str) {
+  let hour = parseInt(str.split(':')[0])
+  if (hour < 12) return 'Good Morning';
+  else if (hour >= 12 && hour < 17) return 'Good Afternoon';
+  else return 'Good Evening';
+}
 /* Write your implementation of displayMessage() */
+
+const displayMessage = str => {
+  let greeting = document.querySelector("h1#greeting");
+  greeting.innerText = str;
+}
